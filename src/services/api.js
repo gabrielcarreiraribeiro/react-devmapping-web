ee1c8159f46519a163cad2ab376b316497d0e897
itsmegabrielr <gabriel.ribeiro@ipremios.com.br>
2020-01-25 20:08:48 -0300

    Changes in baseURL.

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 // Configurando o axios para realizar requisições a partir de uma url base
 const api = axios.create({
-    baseURL: "http://localhost:3001"
+    baseURL: "https://devmappingapi.herokuapp.com"
 })
 
 export default api
